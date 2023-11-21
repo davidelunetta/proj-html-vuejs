46411e8afb561c509d7f6d792f63cb04f308c9c5
davidelunetta <davidelunetta.27@gmail.com>
2023-11-21 23:24:22 +0100

    header structure modified

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    <img src="../assets/img/construction_logo.png" alt="logo" />
-    <nav class="">
-      <ul class="">
-       <li v-for="item in menuItems"><a href="#">{{ item }}</a></li>
-      </ul>
-    </nav>
-  </div>
-  <div id="hero" class="slider d-flex flex-column align-items-center">
-    <h1>Building Inspiring Spaces</h1>
-    <h5>We Build Inspiring Residential & Commercial Spaces</h5>
+  <div class="d-flex justify-content-between">
     <div>
+      <img src="../assets/img/construction_logo.png" alt="logo" />
+    </div>
+    <div class="d-flex flex-row">
+      <nav class="">
+        <ul class="">
+          <li v-for="item in menuItems">
+            <a href="#">{{ item }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <div
+    id="hero"
+    class="slider d-flex flex-column align-items-center justify-content-center"
+  >
+    <h1>Building Inspiring Spaces</h1>
+    <h5 class="pt-3">We Build Inspiring Residential & Commercial Spaces</h5>
+    <div class="pt-3">
       <button class="btn-header-right">Explore recent work</button>
       <button class="btn-header-left">Get a quote today</button>
     </div>
@@ -22,8 +31,8 @@
       >Do you have a construction project we can help with?</span
     >
   </div>
-  <div class="d-flex align-items-center justify-content-center ">
-      <button>GET A FREE QUOTE</button>
+  <div class="d-flex align-items-center justify-content-center">
+    <button class="btn-free-quote">GET A FREE QUOTE</button>
   </div>
 </template>
 
@@ -33,7 +42,7 @@ export default {
 
   data() {
     return {
-      menuItems: ["Home", "About", "Services", "Contact"],
+      menuItems: ["Home", "About", "Services", "Work", "Articles", "Get Quote"],
     };
   },
 };

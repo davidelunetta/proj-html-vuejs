46c9963f0fefb87b35131a8614d6fc2e4ff51101
davidelunetta <davidelunetta.27@gmail.com>
2023-11-21 15:50:00 +0100

    added store

---
+++ src/assets/data/store.js
@@ -0,0 +1,10 @@
+import { reactive } from "vue";
+
+new Vue({
+    el: "#app",
+    data: {
+      
+      menuItems: ["Home", "About", "Services", "Contact"],
+    },
+    // Eventuali metodi o altri attributi che potresti necessitare
+  });

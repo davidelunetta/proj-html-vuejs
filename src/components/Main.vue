bd6845d8dc4a94f8bb96534b9f2c662f4e326274
davidelunetta <davidelunetta.27@gmail.com>
2023-11-22 11:30:53 +0100

    main component updated

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="pt-5 pb-5 d-flex flex-column justify-content-center align-items-center"
+  >
+    <span class="fw-medium fs-3"> Specialists in modern construction </span>
+    <div class="separator mt-3"></div>
+    <div class="container mt-3">
+      <span class="text-center"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+        distinctio doloribus quidem odio, ipsum hic facere nesciunt consequuntur
+        laudantium corrupti explicabo recusandae aut consequatur nam magnam
+        doloremque architecto enim magni.
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+
+  data() {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/main.scss";
+</style>

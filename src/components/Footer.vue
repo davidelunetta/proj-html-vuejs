a7b2c6811da842988e6ce3104ea9a00631fcb527
davidelunetta <davidelunetta.27@gmail.com>
2023-11-23 16:41:25 +0100

    call to action updated started footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="call-to-action">
-    <div
-      class="cta-right pt-5 pb-5 d-flex justify-content-start align-items-start"
-    >
+  <div class="call-to-action d-flex justify-content-between">
+    <div class="cta-right pt-5 pb-5 align-items-center">
       <div class="">
         <span class="fw-medium fs-3 text-light"> Trusted Partners </span>
         <div class="separator mt-3"></div>
-
         <span class="text-light mt-3"
           >Vivamus sed consectetur tellus dolor sit amet, consectetur adipiscing
           lorem elit sadip ipsum ets ficilis uns.
         </span>
       </div>
     </div>
-    <div>caioooo</div>
+    <div class="cta-right pt-5 pb-5 d-flex flex-column text-light">
+      <button>Get a Free Quote</button>
+      <span class="text-center pt-3">Only takes a few seconds!</span>
+    </div>
+  </div>
+
+  <div class="my-footer d-flex">
+    <div>a</div>
+    <div>b</div>
+    <div>c</div>
+    <div>d</div>
   </div>
 </template>
 

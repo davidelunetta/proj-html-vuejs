dc3895fd35ec6002bf78cc074fabdc1da7f94aeb
davidelunetta <davidelunetta.27@gmail.com>
2023-11-23 16:22:30 +0100

    created footer component, updated call to action

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Header></Header>
     <Card></Card>
     <Main></Main>
+    <Footer></Footer>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
 import Card from "./components/Card.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
     Header,
     Main,
     Card,
+    Footer,
   },
   data() {
     return {};
---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="call-to-action">
+    <div
+      class="cta-right pt-5 pb-5 d-flex justify-content-start align-items-start"
+    >
+      <div class="">
+        <span class="fw-medium fs-3 text-light"> Trusted Partners </span>
+        <div class="separator mt-3"></div>
+
+        <span class="text-light mt-3"
+          >Vivamus sed consectetur tellus dolor sit amet, consectetur adipiscing
+          lorem elit sadip ipsum ets ficilis uns.
+        </span>
+      </div>
+    </div>
+    <div>caioooo</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/main.scss";
+</style>

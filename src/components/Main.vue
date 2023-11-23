ea22d57f327453e078d9c24a8fe4da49c1304ca3
davidelunetta <davidelunetta.27@gmail.com>
2023-11-23 09:40:59 +0100

    updated latest news

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -141,6 +141,20 @@
       ></button>
     </div>
   </div>
+  <div
+    class="pt-5 pb-5 d-flex flex-column justify-content-center align-items-center text-center"
+  >
+    <span class="fw-medium fs-3"> Latest News </span>
+    <div class="separator mt-3"></div>
+    <div class="container mt-3">
+      <span class="text-center"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+        distinctio doloribus quidem odio, ipsum hic facere nesciunt consequuntur
+        laudantium corrupti explicabo recusandae aut consequatur nam magnam
+        doloremque architecto enim magni.
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
